07f221c0615080668e9db5713941bf647dd02d3e
coltonsteeve <coltonsteeve@gmail.com>
2019-04-04 13:38:50 -0500

    Added a jumbotron and buttons

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 /* Module 5 Project Script File */
+var jsCat = [{}, {question: "Bob", answer = "CS"}, {name: "Susan", major="IDM:CS"}, {name: "Jane", major: "IDM:VI"} ];  
+var jqCat = [{}, {name: "Bob", major = "CS"}, {name: "Susan", major="IDM:CS"}, {name: "Jane", major: "IDM:VI"} ];
+var bsCat = [{}, {name: "Bob", major = "CS"}, {name: "Susan", major="IDM:CS"}, {name: "Jane", major: "IDM:VI"} ];
+
+function showQuestion (category, amount) { 
+    var index = amount/100;
+    if (category == "jsCat") {
+        
+    }
+    else if (category == "jqCat") {
+        
+    }
+    else {
+
+    }
+ }
